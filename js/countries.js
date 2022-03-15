63c09a35b3108b7bdb0b7e3396c7bcb8d8d5ad72
chamakkarmakar <chamakkarmakar.ck@gmail.com>
2022-03-15 16:31:27 +0600

    load countries

--- js/countries.js
+++ js/countries.js
@@ -1,1 +1,23 @@
-console.log('connected');
+const loadCountries = () => {
+    const url = 'https://restcountries.com/v3.1/all';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountries(data));
+}
+const displayCountries = countries =>{
+    // console.log(countries[0]);
+    const container = document.getElementById('countries');
+    const countriesHTML = countries.map(country => {
+        const display = `
+        <div class="country">
+        <h2>${country.name.common}</h2>
+        <img src="${country.flags.png}" alt="">
+
+    </div>
+        `
+        return display;
+    });
+    // console.log(countriesHTML);
+    container.innerHTML=countriesHTML.join(' ');
+}
+loadCountries();
